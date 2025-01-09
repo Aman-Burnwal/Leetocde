5a80b506abb9a353416de59a14e1406e3ded97c3
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-01-09 17:32:54 +0530

    Time: 21 ms (6.65%) | Memory: 55.8 MB (5.20%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.js
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.js
@@ -4,12 +4,57 @@
  * @return {number}
  */
 var prefixCount = function(words, pref) {
-    let count =0, len = pref.length;
+    
+    const root = new createTrie();
 
     for(const word of words) {
 
-        if(word.includes(pref) && word.indexOf(pref) == 0)count++;
+        insertTrie(word, root);
     }
 
-    return count
+    return searchTrie( root, 0);
+
+   function searchTrie(root, i) {
+     
+        if (i === pref.length)  return root.count;
+        
+        let idx = pref.charCodeAt(i) - 97;
+
+        if (!root.children[idx]) return 0;
+        
+        return searchTrie(root.children[idx], i + 1);
+    }
+    
+
+    // create trie
+    function createTrie() {
+
+        return {
+            children: new Array(26).fill(null),
+            count:0,
+        }
+    }
+
+    // insert word in Trie
+    function insertTrie( word, root) {
+
+        let crawsal = root;
+        let idx = 0;
+
+        for(const ch of word) {
+
+            idx = ch.charCodeAt(0) - 97;
+
+            if(!crawsal.children[idx]) {
+                crawsal.children[idx] = createTrie();
+            }
+            crawsal = crawsal.children[idx];
+            crawsal.count++;
+            
+
+        }
+       
+
+    }
+
 };
