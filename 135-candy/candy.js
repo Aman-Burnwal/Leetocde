92d6aeef48ba69a5cd433037ac386908154f19d3
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-01-09 21:04:07 +0530

    Time: 17 ms (15.38%) | Memory: 53.4 MB (59.15%) - LeetSync

--- 135-candy/candy.js
+++ 135-candy/candy.js
@@ -4,25 +4,40 @@
  */
 var candy = function(ratings) {
 
-    const cand = new Array(ratings.length).fill(1);
+    const cand = new Array(ratings.length);
+
+
+    for(let i = 0; i < ratings.length; i++) {
+
+        cand[i] = 1;
+    }
 
 
     for(let i = 1; i < ratings.length; i++) {
 
-        if(ratings[i] > ratings[i - 1]) cand[i] = Math.max(cand[i - 1] + 1, cand[i])
+        if(ratings[i] > ratings[i - 1]) {
+             cand[i] = Math.max(cand[i - 1] + 1, cand[i])
+
+        }
     }
 
     for(let i = ratings.length - 2; i >= 0; i--) {
 
-        if(ratings[i] > ratings[i +1]) cand[i] = Math.max(cand[i + 1] + 1, cand[i])
+        if(ratings[i] > ratings[i +1]) {
+            cand[i] = Math.max(cand[i + 1] + 1, cand[i])
+    
+        }
     }
 
 
     let sum = 0;
-    // console.log(cand)
+   
 
 
-    for(const can of cand) sum += can;
+    for(let c of cand) {
+
+        sum += c;
+    }
     return sum;
     
 };
