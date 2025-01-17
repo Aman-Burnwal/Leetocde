53e91c3e0e35351dd2e88d4ac0bce0fe0123a64c
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-01-17 09:18:58 +0530

    Time: 6 ms (25.00%) | Memory: 65.5 MB (75.00%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} derived
+ * @return {boolean}
+ */
+var doesValidArrayExist = function(derived) {
+
+
+    return (helper(0) || helper(1))
+
+    function helper(bit) {
+        let b = bit;
+
+        for(let i = 0; i < derived.length - 1; i++) bit ^= derived[i];
+
+        return b ^ bit == derived[derived.length - 1];
+    }
+
+    
+};
