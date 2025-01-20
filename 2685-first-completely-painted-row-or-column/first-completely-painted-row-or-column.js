b58174f6c801ac7062402b30a54a1f65b7ae82d4
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-01-20 15:44:45 +0530

    Time: 192 ms (16.67%) | Memory: 89.4 MB (16.67%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.js
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.js
@@ -7,7 +7,7 @@ var firstCompleteIndex = function(arr, mat) {
 
 
     let map = new Map() 
-    const cMap = new Map();
+
     let len = mat.length;
 
 
@@ -15,31 +15,28 @@ var firstCompleteIndex = function(arr, mat) {
 
         for(let j = 0; j < mat[0].length; j++) {
 
-            map.set(mat[i][j], i);
-            cMap.set(mat[i][j], j);
+            map.set(mat[i][j], [i, j]);
+            
         }
     }
 
     const freq = new Array(len).fill(0);
     const freq2 = new Array(mat[0].length).fill(0);
 
-    console.log(map, cMap)
+
 
 
     for(let i = 0; i < arr.length; i++) {
 
-        let idx = map.get(arr[i]);
+        const [idx, idx1] = map.get(arr[i]);
 
         freq[idx]++;
-
-        // console.log(freq);
-
         if(freq[idx] == mat[0].length) return i;
-        idx = cMap.get(arr[i]);
+        
 
-        freq2[idx]++;
-        // console.log(freq2, i)
-        if(freq2[idx] == len) return i;
+        freq2[idx1]++;
+       
+        if(freq2[idx1] == len) return i;
     }
 
 
