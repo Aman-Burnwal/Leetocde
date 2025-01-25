bf521b43d8bfd30a983f0413b7aca2505ca8972d
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-01-25 12:28:32 +0530

    Time: 56 ms (14.67%) | Memory: 62.4 MB (20.65%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.js
+++ 820-find-eventual-safe-states/find-eventual-safe-states.js
@@ -3,12 +3,65 @@
  * @return {number[]}
  */
 var eventualSafeNodes = function(graph) {
-
+    
     const ans = new Array();
 
     const visited = new Array(graph.length).fill(false);
 
     const inR = new Array(graph.length).fill(false);
+
+    const adj = Array.from({length: graph.length}, () => new Array())
+
+    for(let i = 0; i < graph.length; i++) {
+
+        for(let j = 0; j < graph[i].length; j++) {
+
+            adj[graph[i][j]].push(i);
+        }
+    }
+
+    const freq = new Array(graph.length).fill(0);
+
+
+    for(const u of adj) {
+
+        for(const  v of u) freq[v]++;
+    }
+
+    const queue = new Array();
+
+    for(let i = 0; i < freq.length; i++) {
+
+        if(freq[i] == 0) queue.push(i)
+    }
+
+    let i = 0;
+    
+    while(i < queue.length) {
+
+        let u = queue[i++];
+        ans.push(u);
+
+        for(const v of adj[u]) {
+
+            freq[v]--;
+
+            if(freq[v] == 0) queue.push(v);
+        }
+    }
+
+    return ans.sort((a, b) => a - b);
+
+    
+
+
+
+    console.log(freq)
+
+
+
+
+
 
 
     for(let i = 0 ; i < graph.length; i++) {
