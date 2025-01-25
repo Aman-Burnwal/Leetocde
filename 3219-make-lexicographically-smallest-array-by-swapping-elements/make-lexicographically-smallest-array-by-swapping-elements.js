e409f633d05b701e3da1f14fe1d89e7cc0b7b9df
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-01-25 18:03:50 +0530

    Time: 238 ms (100.00%) | Memory: 102.3 MB (18.18%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.js
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.js
@@ -9,17 +9,20 @@ var lexicographicallySmallestArray = function(nums, limit) {
     const copyNums = [...nums].sort((a, b) => a - b);
     const number_map = new Map()
     const Number_to_index_map = new Map();
+
+
     let indx = 0;
 
-    Number_to_index_map.set(copyNums[0], 0)
- 
+    Number_to_index_map.set(copyNums[0], indx)
     number_map.set(indx, [copyNums[0]]);
+ 
 
 
     for(let i = 1; i < nums.length; i++) {
 
         if(Math.abs(copyNums[i] - copyNums[i - 1]) > limit){
             indx++;
+        
             number_map.set(indx, [])
         }
         number_map.get(indx).push(copyNums[i]);
@@ -27,10 +30,14 @@ var lexicographicallySmallestArray = function(nums, limit) {
 
     }
 
+    const i_map= new Array(indx + 1).fill(0);
+
     for(let i = 0; i < nums.length; i++) {
 
         indx =  Number_to_index_map.get(nums[i]);
-        nums[i] = number_map.get(indx).shift();
+
+        nums[i] = number_map.get(indx)[i_map[indx]++]
+        
 
     }
     return nums;
