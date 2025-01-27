1f8f3cac6bf1655fa6c04655e79ebedc8f2cb99a
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-01-27 09:21:47 +0530

    Time: 81 ms (8.62%) | Memory: 62 MB (5.11%) - LeetSync

--- 207-course-schedule/course-schedule.js
+++ 207-course-schedule/course-schedule.js
@@ -7,45 +7,82 @@ var canFinish = function(n, prerequisites) {
 
 
     const map = new Map();
-    const indegree = new Array(n).fill(0);
+    
 
 
     for(const [u, v] of prerequisites) {
 
         if(!map.has(v))map.set(v, []);
         map.get(v).push(u);
-        indegree[u]++;
+        // indegree[u]++;
     }
 
 
-    const que = new Array();
-
+    const visited = new Array(n).fill(false);
+    console.log(map)
 
     for(let i = 0; i < n; i++) {
-        if(indegree[i] == 0) que.push(i)
-    }
+        const inRecursion = new Array(n).fill(false);
+        
+        // console.log(i,visited[i],  got)
+        if(!visited[i] ) {
 
-
-    while(que.length) {
-
-        const u = que.pop();
-
-        if(!map.has(u)) continue;
-        // const arr = map.get(u)
-        // console.log
-        for(const v of map.get(u)) {
-
-            indegree[v]--;
-
-            if(indegree[v] == 0) que.push(v); 
+            let got = DFS(i, inRecursion)
+            console.log(got)
+            if(got) return false;
         }
     }
+    return true;
+    function DFS(u, inRecursion) {
 
-    for(let i = 0; i < n; i++) {
-        if(indegree[i] != 0) return false;
+        if(inRecursion[u]) return false;
+
+        visited[u] = true;
+        inRecursion[u] = true;
+
+        if(map.has(u)) {
+
+            for(const v of map.get(u)) {
+
+                if(!visited[v] && DFS(v , inRecursion)) return true;
+                else if(inRecursion[v]) return true;
+            }
+        }
+        inRecursion[u] = false;
+
+        return false;
     }
 
-    return true;
+    
+
+    // const indegree = new Array(n).fill(0);
+    // const que = new Array();
+
+
+    // for(let i = 0; i < n; i++) {
+    //     if(indegree[i] == 0) que.push(i)
+    // }
+
+
+    // while(que.length) {
+
+    //     const u = que.pop();
+
+    //     if(!map.has(u)) continue;
+
+    //     for(const v of map.get(u)) {
+
+    //         indegree[v]--;
+
+    //         if(indegree[v] == 0) que.push(v); 
+    //     }
+    // }
+
+    // for(let i = 0; i < n; i++) {
+    //     if(indegree[i] != 0) return false;
+    // }
+
+    // return true;
     
     
 };
