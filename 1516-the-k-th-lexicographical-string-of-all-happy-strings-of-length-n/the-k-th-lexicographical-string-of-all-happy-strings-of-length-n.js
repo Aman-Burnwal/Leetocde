f45bd6b6c5daf2109c339cc8a91247ddf00ca588
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-02-19 19:50:02 +0530

    Time: 2 ms (100.00%) | Memory: 57 MB (9.30%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getHappyString = function(n, k) {
+
+    let count = 0;
+    let ans = "";
+    
+
+    function generater (stack) {
+    
+        if(stack.length == n) {
+            count++;
+            count == k ? ans = stack.join(""): "";
+            
+            return;
+        }
+        if(ans != "") return;
+        
+        for(const ch of "abc") {
+            stack.push(ch);
+            if(stack.length == 1) generater(stack);
+            else if(stack[stack.length - 2] != ch) generater(stack);
+            stack.pop();
+        }
+    }
+
+    generater([]);
+    return ans;
+};
