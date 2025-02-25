a0f2df7d3cf14c134034b058404c69ed25cf0168
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-02-26 00:51:11 +0530

    Time: 6 ms (89.51%) | Memory: 58.1 MB (7.56%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.js
@@ -4,10 +4,50 @@
  */
 var maximumProduct = function(nums) {
     
-    nums.sort((a, b) => a - b);
-    let n = nums.length - 1;
+    
 
-    let max = Math.max(nums[n] * nums[n - 1] * nums[n - 2], Math.max(nums[n] * nums[n - 1] * nums[0], nums[n] * nums[0] * nums[1]));
 
-    return Math.max(max, nums[0] * nums[1] * nums[2]);
+    let min1 = Number.MAX_SAFE_INTEGER, min2 = min1, min3 = min2;
+    let max1 = Number.MIN_SAFE_INTEGER, max2 = max1, max3 = max1;
+
+
+
+    for(const n of nums) {
+
+        if(n > max1) {
+            
+            max3 = max2;
+            max2 = max1;
+            max1 = n;
+        }
+
+        else if (n > max2) {
+            max3 = max2;
+            max2 = n;
+        }
+        else if (n > max3) max3 = n;
+
+        if (n < min1) {
+            min3 = min2;
+            min2 = min1;
+            min1 = n;
+        }
+        else if (n < min2) {
+            min3 = min2;
+            min2 = n;
+        }
+        else if(n < min3) min3 = n;
+    }
+   
+    let mul1 = max1 * max2 * max3;
+    let mul2 = max1 * max2 * min1;
+    let mul3 = max1 * min1 * min2;
+    let mul4 = min1 * min2 * min3
+
+
+
+    let max = Math.max(mul1,  Math.max(mul2,  Math.max(mul3, mul4)));
+
+    
+    return max;
 };
