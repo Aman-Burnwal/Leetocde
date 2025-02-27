9e9921b12e721b7961ffe4eea9d548fbb57f3dbb
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-02-28 02:16:57 +0530

    Time: 386 ms (31.58%) | Memory: 84.4 MB (5.26%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.js
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.js
@@ -14,16 +14,28 @@ var lenLongestFibSubseq = function(arr) {
         map.set(arr[i], i);
     }
     
-    const memo = Array.from({length : 1001}, () => new Array());
+    const memo = Array.from({length : arr.length}, () => new Array(arr.length).fill(2));
 
 
     for(let i = 1; i < arr.length; i++) {
 
         for(let j = i + 1; j < arr.length; j++) {
 
-            len = Math.max(len, solve(i, j));
+            // len = Math.max(len, solve(i, j));
+
+            if(map.has(arr[j] - arr[i]) && map.get(arr[j] - arr[i]) < i) {
+
+                let k = map.get( arr[j] - arr[i] );
+
+               
+                memo[i][j] = 1 + memo[k][i]
+
+                len = Math.max(memo[i][j], len);
+            } 
         }
     }
+
+    return len > 2 ? len : 0;
 
 
 
