230b25d99c7c6004218b9c2050ae26e79f1c156b
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-03-06 02:14:25 +0530

    Time: 1 ms (99.44%) | Memory: 54.3 MB (42.39%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.js
+++ 10-regular-expression-matching/regular-expression-matching.js
@@ -4,11 +4,11 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-    // let memo = new Map();
+    let memo = new Map();
     
     function helper(i, j) {
-        // let key = `${i}-${j}`;
-        // if (memo.has(key)) return memo.get(key);
+        let key = `${i}-${j}`;
+        if (memo.has(key)) return memo.get(key);
         
         if (j === p.length) return i === s.length;
 
@@ -16,13 +16,13 @@ var isMatch = function(s, p) {
         let firstMatch = (i < s.length) && (s[i] === p[j] || p[j] === '.');
 
         if (j + 1 < p.length && p[j + 1] === '*') {
-            
+
             let ans = helper(i, j + 2) || (firstMatch && helper(i + 1, j));
-            // memo.set(key, ans);
+            memo.set(key, ans);
             return ans;
         } else {
             let ans = firstMatch && helper(i + 1, j + 1);
-            // memo.set(key, ans);
+            memo.set(key, ans);
             return ans;
         }
     }
