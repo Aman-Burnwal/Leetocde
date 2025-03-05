d2570887186fe2b2db54e9dc5790ba31c2a2c490
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-03-05 10:37:41 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (26.32%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.js
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.js
@@ -3,16 +3,8 @@
  * @return {number}
  */
 var coloredCells = function(n) {
+    
 
-
-    let cells = 1;
-
-
-    for(let i = 2; i <= n; i++) {
-
-        cells += (i - 1) * 4;
-    }
-
-    return cells;
+    return 1 +  2 * ((n - 1) * n);
     
 };
