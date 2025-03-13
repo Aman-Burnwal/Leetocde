3d9ff6d7904361cb2d9edf1ae7cec7d5e202418e
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-03-13 20:41:45 +0530

    Time: 169 ms (23.53%) | Memory: 100.4 MB (17.65%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.js
@@ -0,0 +1,70 @@
+/**
+ * @param {number[]} nums
+ * @param {number[][]} queries
+ * @return {number}
+ */
+var minZeroArray = function(nums, queries) {
+
+    let is = true;
+
+    for(const num of nums) {
+        if(num) {
+            is = false;
+            break;
+        }
+    }
+
+    if(is) return 0;
+    
+    
+
+
+    let start = 0;
+    
+    let end = queries.length - 1;
+    let ans = 0;
+
+    while(start <= end) {
+
+        let mid = (start + end) >> 1;
+
+        if(check(mid))  {
+            ans = mid;
+            end = mid - 1 ;
+
+        }
+        else start = mid + 1;
+    }
+    
+
+    return is ?  ans + 1 : -1 ;
+
+    function check (mid) {
+
+        console.log(mid);
+
+        const arr = new Array(nums.length + 1).fill(0);
+
+        for(let i = 0; i <= mid; i++) {
+
+            const [start, end, v] = queries[i];
+
+            arr[start] -= v;
+            arr[end + 1] += v;
+
+        }
+        let pre = 0;
+
+        for(let i = 1; i <= nums.length; i++) {
+
+        
+            arr[i] += arr[i - 1];
+            
+
+            if((nums[i - 1] + arr[i - 1]) >  0) return false;
+        }
+        is = true;
+        return true;
+    }
+    
+};
