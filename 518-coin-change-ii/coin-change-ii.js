c0ad1ecb5c6552389ad2be7555ab786d3ae73f0c
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-03-18 16:29:06 +0530

    Time: 59 ms (37.48%) | Memory: 80.4 MB (31.54%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+    
+    let count = 0;
+    const dp = Array.from({length: amount + 2}, () => new Array(coins.length + 1).fill(-1))
+
+    count = helper(0, 0);
+    
+
+    return count ;
+
+    function helper(sum , i) {
+
+       
+        if(sum == amount)  return 1;
+    
+        if(sum > amount || i >= coins.length) return 0;
+        if(dp[sum][i] != -1) return dp[sum][i];
+
+
+        let a = 0;
+        b = 0;
+
+        if(amount >= (coins[i] + sum )) a =  helper(sum +coins[i] , i );
+        b =  helper(sum, i + 1);
+
+        return dp[sum][i] = a + b;
+    }
+};
