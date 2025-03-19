99ab2817579c02d84d0cc54c1f68e39583161e63
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-03-19 17:31:25 +0530

    Time: 6 ms (80.00%) | Memory: 64.3 MB (40.00%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
@@ -10,8 +10,8 @@ var minOperations = function(nums) {
 
         if(nums[i] == 0) {
             count++;
-            nums[i + 1] = nums[i + 1] == 0 ? 1 : 0;
-            nums[i + 2] = nums[i + 2] == 0 ? 1 : 0;
+            nums[i + 1] ^= 1;
+            nums[i + 2] ^= 1;
         }
     }
 
