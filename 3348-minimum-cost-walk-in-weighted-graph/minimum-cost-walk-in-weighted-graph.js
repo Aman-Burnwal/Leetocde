0408f8bf7a771848aaceedec38b21f076ad5ce86
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-03-20 20:32:41 +0530

    Time: 136 ms (15.38%) | Memory: 116.1 MB (7.69%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.js
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.js
@@ -5,68 +5,53 @@
  * @return {number[]}
  */
 var minimumCost = function(n, edges, query) {
+    const parent = Array.from({ length: n }, (_, i) => i);
+    const rank = new Array(n).fill(1);
+    const andValue = new Array(n).fill(-1);
 
-    const parent = new Array(n );
-    const andValue = new Array(n ).fill(-1);
-    const ans = new Array();
-
-
-    for(let i = 0; i <  parent.length; i++) {
-        parent[i] = i;
+    function findParent(num) {
+        if (parent[num] !== num) {
+            parent[num] = findParent(parent[num]); // Path compression
+            andValue[parent[num]] &= andValue[num]; // Update andValue for compression
+        }
+        return parent[num];
     }
 
-    for(const [u, v, w] of edges) {
+    function union(x, y, w) {
+        let rootX = findParent(x);
+        let rootY = findParent(y);
 
-        andValue[u] != -1 ? andValue[u] &= w : andValue[u] = w;
-        andValue[v] != -1 ? andValue[v] &= w :  andValue[v] = w;
+        if (rootX !== rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+                andValue[rootX] &= (andValue[rootY] !== -1 ? andValue[rootY] : w);
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+                andValue[rootY] &= (andValue[rootX] !== -1 ? andValue[rootX] : w);
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+                andValue[rootX] &= (andValue[rootY] !== -1 ? andValue[rootY] : w);
+            }
+        }
     }
 
-
-
-    for(const [u, v, w] of edges) {
-
-        let x = findParent(u);
-        let y = findParent(v);
-
-        if(parent[x] > parent[y])  parent[x] = y;
-        
-        else parent[y] = x;
-         
-
+    // Initialize andValue with edge weights
+    for (const [u, v, w] of edges) {
+        andValue[u] = andValue[u] === -1 ? w : andValue[u] & w;
+        andValue[v] = andValue[v] === -1 ? w : andValue[v] & w;
     }
-    
-    for(let i = 0; i < n; i++) {
 
+    // Union-Find to connect components
+    for (const [u, v, w] of edges) {
+        union(u, v, w);
+    }
+
+    // Find parents after all unions
+    for (let i = 0; i < n; i++) {
         findParent(i);
     }
 
-    for(const [u, v] of query) {
-
-        let x = findParent(u);
-        let y = findParent(v);
-
-        ans.push( x != y ? -1 : andValue[x]);
-      
-        
-    }
-
-
-
-
-    return ans;
-
-
-    
-    function findParent(num) {
-
-        if(parent[num] == num) return num;
-
-        andValue[parent[num]] &= andValue[num]; 
-
-        return parent[num] = findParent(parent[num]);
-    }
-
-    
-
-    
+    // Process queries
+    return query.map(([u, v]) => findParent(u) === findParent(v) ? andValue[findParent(u)] : -1);
 };
