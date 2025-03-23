38b2dcc2f048ffa5cf4acc2cca60bbb478c1b1cc
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-03-23 15:11:27 +0530

    Time: 83 ms (18.42%) | Memory: 67.2 MB (5.26%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.js
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.js
@@ -1,42 +1,78 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
 var countCompleteComponents = function(n, edges) {
+
+
+
     let count = 0;
-    const parent = new Array(n).fill(0).map((_, i) => i);
-    const size = new Array(n).fill(1);
-    const edgeCount = new Array(n).fill(0);
+    const parent = new Array();
+    const size  = new Array(n).fill(1);
+    const mp = new Map();
 
-    function find(x) {
-        return parent[x] === x ? x : (parent[x] = find(parent[x])); // Path Compression
-    }
+    for(let i = 0; i < n; i++) parent.push(i);
 
-    function union(x, y) {
-        let x_ = find(x);
-        let y_ = find(y);
-        if (x_ === y_) return;
+    for(const [u, v] of edges) {
 
-        if (size[x_] > size[y_]) {
-            parent[y_] = x_;
-            size[x_] += size[y_];
-        } else {
-            parent[x_] = y_;
-            size[y_] += size[x_];
-        }
-    }
-
-    for (const [u, v] of edges) {
         union(u, v);
     }
 
-    for (const [u, v] of edges) {
-        edgeCount[find(u)]++;  // Directly store edge count
+    for(const [u, v] of edges) {
+
+       let idx =  find(u);
+
+       mp.set(idx, (mp.get(idx) || 0) + 2);
     }
 
-    for (let i = 0; i < n; i++) {
-        if (find(i) === i) { // Checking root node
-            if (size[i] === 1 || edgeCount[i] === (size[i] * (size[i] - 1)) / 2) {
-                count++;
-            }
+    for(let i = 0; i < n; i++) {
+
+        if(parent[i] == i) {
+          
+          if(size[i] == 1) {
+            count++;
+            continue;
+          }
+          count +=  ( size[i] * (size[i] - 1)) ==( mp.get(i) ) ? 1 : 0;
+          console.log(i, count)
         }
     }
 
+
     return count;
+
+
+    function find(x) {
+
+        if(parent[x] == x) return x;
+
+        return parent[x] = find(parent[x]);
+
+    }
+
+
+    function union(x, y) {
+
+        let x_ = find(x);
+        let y_ = find(y);
+
+
+        if(x_ == y_ ) return;
+
+        if(size[x_] > size[y_]) {
+
+          parent[y_] = x_;
+          size[x_] += size[y_]
+        //   mp.set(x_, (mp.get(x_) || 0) + 1);
+        }
+        else {
+            parent[x_] = y_;
+            size[y_] += size[x_];
+            // mp.set(x_, (mp.get(x_) || 0) + 1);
+        }
+    }
+
+
+    
 };
