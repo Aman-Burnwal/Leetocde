eefeb6d8b7186eb6378bb3077c73c12daa1e95f6
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-03-23 12:43:41 +0530

    Time: 73 ms (68.00%) | Memory: 76.6 MB (16.00%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.js
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.js
@@ -4,40 +4,61 @@
  * @return {number}
  */
 var countPaths = function(n, roads) {
-    const mod = 1e9 + 7;
+    
     const adj = new Map();
+    const result = new Array(n).fill(Number.MAX_SAFE_INTEGER);
+    const countPath = new Array(n).fill(0);
+    const pq = new Array();
+    let mod = 10e8 + 7;
+    pq.push([0, 0]) // node, dist
+    countPath[0] = 1;
+    result[0] = 0;
 
-    for (const [u, v, w] of roads) {
-        if (!adj.has(u)) adj.set(u, []);
+
+    for(const [u, v, w] of roads) {
+
+        if(!adj.has(u)) adj.set(u, []);
+        if(!adj.has(v)) adj.set(v, []);
+
         adj.get(u).push([v, w]);
-        if (!adj.has(v)) adj.set(v, []);
         adj.get(v).push([u, w]);
     }
 
-    const dist = new Array(n).fill(Number.MAX_SAFE_INTEGER);
-    const ways = new Array(n).fill(0);
-    
-    dist[0] = 0;
-    ways[0] = 1;
+    while(pq.length) {
 
-    let minHeap = [[0, 0]]; // [distance, node]
+       
+        pq.sort((a, b) => a[1] - b[1]);
+        
+        const [currNode, currTime] = pq.shift();
 
-    while (minHeap.length) {
-        minHeap.sort((a, b) => a[0] - b[0]);
-        let [w1, u] = minHeap.shift();
+        if(!adj.has(currNode)) continue;
 
-        if (!adj.has(u)) continue;
+        for(const [nextNode , nextTime] of adj.get(currNode)) {
 
-        for (const [v, w] of adj.get(u)) {
-            if (w1 + w < dist[v]) {
-                dist[v] = w1 + w;
-                ways[v] = ways[u];
-                minHeap.push([dist[v], v]);
-            } else if (w1 + w === dist[v]) {
-                ways[v] = (ways[v] + ways[u]) % mod;
+           
+
+            if(currTime + nextTime < result[nextNode]) {
+                
+                result[nextNode] = currTime + nextTime;
+
+                countPath[nextNode] = countPath[currNode];
+
+                pq.push([nextNode, result[nextNode]]);
+
+            }
+            else if(currTime + nextTime == result[nextNode]) {
+               
+                countPath[nextNode] = (countPath[nextNode] + countPath[currNode]) %mod;
+          
             }
         }
+        // console.log(currTime, currNode, pq)
+        // console.log(result)
+        // console.log(countPath)
+        
     }
+   
 
-    return ways[n - 1] % mod;
+    return (countPath[n - 1]) % mod;
+
 };
