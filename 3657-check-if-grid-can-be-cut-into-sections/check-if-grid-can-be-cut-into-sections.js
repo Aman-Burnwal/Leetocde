64d30218450022e9efabb532e868ceb967f3a0bf
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-03-25 15:20:11 +0530

    Time: 163 ms (100.00%) | Memory: 95.3 MB (100.00%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.js
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.js
@@ -5,46 +5,47 @@
  */
 var checkValidCuts = function(n, rectangles) {
 
-    const hor = new Array();
-    const ver = new Array();
+    // const hor = new Array();
+    // const ver = new Array();
 
-    for(const[hStart, vStart, hEnd, vEnd] of rectangles) {
+    // for(const[hStart, vStart, hEnd, vEnd] of rectangles) {
 
-        hor.push([hStart, hEnd]);
-        ver.push([vStart, vEnd]);
-    }
-    hor.sort((a, b) => a[0] - b[0]);
-    ver.sort((a, b) => a[0] - b[0]);
+    //     hor.push([hStart, hEnd]);
+    //     ver.push([vStart, vEnd]);
+    // }
+    rectangles.sort((a, b) => a[0] - b[0]);
+    // ver.sort((a, b) => a[0] - b[0]);
 
     
 
     let h = 0;
     
 
-    let max = hor[0][1] - 1;
+    let max = rectangles[0][2] - 1;
 
-    for(let i = 1; i < hor.length; i++) {
+    for(let i = 1; i < rectangles.length; i++) {
 
-        if(hor[i][0] > max) {
+        if(rectangles[i][0] > max) {
             h++;
-            max = hor[i][1] - 1;
+            max = rectangles[i][2] - 1;
             if(h == 2) return true;
         }
-        else max = Math.max(hor[i][1] - 1, max);
+        else max = Math.max(rectangles[i][2] - 1, max);
     }
 
     let v = 0;
+    rectangles.sort((a, b) => a[1] - b[1]);
 
-    max = ver[0][1] - 1;
+    max = rectangles[0][3] - 1;
 
-    for(let i = 1; i < ver.length; i++) {
+    for(let i = 1; i < rectangles.length; i++) {
 
-        if(ver[i][0] > max) {
+        if(rectangles[i][1] > max) {
             v++;
-            max = ver[i][1] -1;
+            max = rectangles[i][3] -1;
             if(v == 2) return true;
         }
-        else max = Math.max(ver[i][1] - 1, max);
+        else max = Math.max(rectangles[i][3] - 1, max);
     }
 
     return false;
