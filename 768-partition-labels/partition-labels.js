b27e1bf14d8e588bb2594eeeff4b248dc8c4f91c
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-03-30 12:54:32 +0530

    Time: 6 ms (51.03%) | Memory: 56.1 MB (21.61%) - LeetSync

--- 768-partition-labels/partition-labels.js
+++ 768-partition-labels/partition-labels.js
@@ -14,14 +14,11 @@ var partitionLabels = function(s) {
     }
 
     let count = 0;
-
-    let max = arr[s.charCodeAt(0) - 97];
-
-    let i = 0;
+    let max = 0;
     const ans = []; 
 
 
-    while(i < s.length) {
+    for(let i = 0; i < s.length; i++) {
 
         let char = s.charCodeAt(i) - 97;
         count++;
@@ -30,10 +27,8 @@ var partitionLabels = function(s) {
         if(i == max ) {
             ans.push(count);
             count = 0;
-
-            let max = arr[s.charCodeAt(i + 1) - 97];
         }
-        i++;
+     
     }
 
 
