3b1c8df02110e0eb47097dc6eafb70109680e5dd
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-04-03 08:46:19 +0530

    Time: 2 ms (90.32%) | Memory: 66.3 MB (80.65%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.js
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.js
@@ -4,17 +4,10 @@
  */
 var maximumTripletValue = function(nums) {
 
-    const maxRight = new Array(nums.length).fill(0);
-    let max = nums[nums.length - 1];
+ 
 
-    // for(let i = nums.length - 1; i >= 0; i--) {
-
-    //     max = Math.max(nums[i], max);
-
-    //     maxRight[i] = max;
-    // }
     let maxDif = 0;
-    max = 0;
+    let max = 0;
     let result = 0;
 
     for(let i = 0; i < nums.length - 1; i++) {
