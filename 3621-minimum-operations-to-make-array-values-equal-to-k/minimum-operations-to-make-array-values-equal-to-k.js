92389b795a7582d6c1a2111f90e8b56e995a164c
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-04-09 14:12:46 +0530

    Time: 52 ms (79.59%) | Memory: 56 MB (73.47%) - LeetSync

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    
+    const set = new Set();
+
+
+    for(const num of nums) {
+
+        if(num < k) return -1
+        if(num > k) set.add(num);
+    }
+
+    return set.size
+};
