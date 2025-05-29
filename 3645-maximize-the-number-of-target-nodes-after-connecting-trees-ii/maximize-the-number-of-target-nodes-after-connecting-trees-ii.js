1648988a62c4d453d483c462da89965d6899c75c
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-05-29 21:16:18 +0530

    Time: 807 ms (0.00%) | Memory: 177 MB (0.00%) - LeetSync

---
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/maximize-the-number-of-target-nodes-after-connecting-trees-ii.js
@@ -0,0 +1,114 @@
+/**
+ * @param {number[][]} edges1
+ * @param {number[][]} edges2
+ * @return {number[]}
+ */
+var maxTargetNodes = function(edges1, edges2) {
+    
+    const adj1 = new Map();
+    const adj2 = new Map();
+
+    createAdjList(edges1, adj1);
+    createAdjList(edges2, adj2);
+
+    const n1 = adj1.size;
+    // const dp = Array.from({length: n2}, () => [false, false])
+    const n2 = adj2.size;
+
+
+    const visited = new Array(Math.max(n2, n1)).fill(0);
+    const ans = new Array()
+    let EvenMax = 0, oddMax = 0, countOdd = 0, countEven = 0;
+
+
+
+    DFSHead(0, 0, adj2);
+    let max = Math.max(countOdd, countEven);
+    countOdd = 0;
+    countEven = 0;
+    Positioning(0, 0);
+    DFSHead(0, 0, adj1);
+
+
+    for(let i = 0; i < n1; i++) {
+
+        // countOdd = 0;
+        
+        ans[i] = !ans[i] ? countEven + max : countOdd + max;
+    }
+    
+
+    //  DFSPosition(curr, u)
+
+        // console.log(oddEven, count)
+
+        function Positioning(curr , u) {
+
+            if(visited[u]) return;
+
+            ans[u] = curr % 2 
+            
+            
+
+            visited[u] = true;
+
+            for(const v of adj1.get(u)) {
+
+                if(visited[v]) continue;
+                Positioning(curr + 1, v);
+            }
+            visited[u] = false;
+
+            return;
+        }
+
+  
+
+
+
+
+    return ans //  change it
+
+    function DFSHead (curr, u, adj2 ) {
+
+        DFS(curr, u)
+
+        // console.log(oddEven, count)
+
+        function DFS(curr , u) {
+
+            if(visited[u]) return;
+
+            curr % 2 ? countOdd++ :  countEven++;
+            
+            
+
+            visited[u] = true;
+
+            for(const v of adj2.get(u)) {
+
+                if(visited[v]) continue;
+                DFS(curr + 1, v);
+            }
+            visited[u] = false;
+
+            return;
+        }
+    }
+
+    
+
+
+    function createAdjList(edges , adj) {
+
+        for(const [u, v] of edges) {
+
+            if(!adj.has(u)) adj.set(u, []);
+            if(!adj.has(v)) adj.set(v, []);
+
+            adj.get(u).push(v);
+            adj.get(v).push(u);
+        }
+    }
+
+};
