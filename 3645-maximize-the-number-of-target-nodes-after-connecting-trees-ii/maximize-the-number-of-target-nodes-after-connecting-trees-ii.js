976854acff36963fdfe04124257064fe681fbb9a
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-05-29 21:46:04 +0530

    Time: 901 ms (0.00%) | Memory: 176.4 MB (0.00%) - LeetSync

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/maximize-the-number-of-target-nodes-after-connecting-trees-ii.js
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/maximize-the-number-of-target-nodes-after-connecting-trees-ii.js
@@ -5,25 +5,45 @@
  */
 var maxTargetNodes = function(edges1, edges2) {
     
+    const n1  = 1 + edges1.map((curr) =>  {
+                    return curr.reduce((a, b) => Math.max(a, b), 0)
+                }).reduce((acc, curr) => Math.max(acc, curr), 0)
+    
+    const n2  = 1 + edges2.map((curr) =>  {
+                    return curr.reduce((a, b) => Math.max(a, b), 0)
+                }).reduce((acc, curr) => Math.max(acc, curr), 0)
+    
     const adj1 = new Map();
     const adj2 = new Map();
+
+
+
+    for(let i = 0; i < n1; i++) {
+        adj1.set(i, []);
+    }
+    
+    for(let i = 0; i < n2; i++) {
+        adj2.set(i, []);
+    }
+
+ 
+
+    
 
     createAdjList(edges1, adj1);
     createAdjList(edges2, adj2);
 
-    const n1 = adj1.size;
-    // const dp = Array.from({length: n2}, () => [false, false])
-    const n2 = adj2.size;
+
 
 
     const visited = new Array(Math.max(n2, n1)).fill(0);
     const ans = new Array()
     let EvenMax = 0, oddMax = 0, countOdd = 0, countEven = 0;
 
-
-
     DFSHead(0, 0, adj2);
+
     let max = Math.max(countOdd, countEven);
+
     countOdd = 0;
     countEven = 0;
     Positioning(0, 0);
@@ -32,15 +52,12 @@ var maxTargetNodes = function(edges1, edges2) {
 
     for(let i = 0; i < n1; i++) {
 
-        // countOdd = 0;
+
         
         ans[i] = !ans[i] ? countEven + max : countOdd + max;
     }
     
 
-    //  DFSPosition(curr, u)
-
-        // console.log(oddEven, count)
 
         function Positioning(curr , u) {
 
@@ -73,7 +90,7 @@ var maxTargetNodes = function(edges1, edges2) {
 
         DFS(curr, u)
 
-        // console.log(oddEven, count)
+
 
         function DFS(curr , u) {
 
@@ -103,8 +120,6 @@ var maxTargetNodes = function(edges1, edges2) {
 
         for(const [u, v] of edges) {
 
-            if(!adj.has(u)) adj.set(u, []);
-            if(!adj.has(v)) adj.set(v, []);
 
             adj.get(u).push(v);
             adj.get(v).push(u);
