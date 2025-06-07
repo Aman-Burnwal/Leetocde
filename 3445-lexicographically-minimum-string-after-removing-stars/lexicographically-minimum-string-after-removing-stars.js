f4ae93805379d1b1b251ced443ff62d9386d6473
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-06-07 17:13:30 +0530

    Time: 1855 ms (0.00%) | Memory: 67.4 MB (40.00%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.js
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.js
@@ -2,37 +2,106 @@
  * @param {string} s
  * @return {string}
  */
-var clearStars = function(s) {
-
-
-    const greadyInd = Array.from({length : 26}, () => []);
-    const ans = s.split("");
-   
-
-    for(let i = 0; i < s.length; i++) {
-
-        if(s[i] == "*"){
-
-            for(let j= 0; j < 26; j++) {
+var clearStars = function(s) 
+{
+    let stack = [];
+    let freq = new Array(26).fill(0); 
+    
+    for (let i = 0; i < s.length; i++) 
+    {
+        if (s[i] === '*') 
+        {
+            if (stack.length > 0) 
+            {
+                let minCharCode = -1;
+                for (let j = 0; j < 26; j++) 
+                {
+                    if (freq[j] > 0) 
+                    {
+                        minCharCode = j;
+                        break;
+                    }
+                }
                 
-                if(greadyInd[j].length > 0) {
-                    ans[greadyInd[j].pop()] = "*";
-                    break;
+                if (minCharCode !== -1) 
+                {
+                    let minChar = String.fromCharCode(minCharCode + 97);
+                    
+                    for (let k = stack.length - 1; k >= 0; k--) 
+                    {
+                        if (stack[k] === minChar) 
+                        {
+                            stack.splice(k, 1);
+                            freq[minCharCode]--;
+                            break;
+                        }
+                    }
                 }
             }
-
-        
-        }      
-        else {
-         
-            const charcode = s.charCodeAt(i) - 97;
-            greadyInd[charcode].push(i);
         } 
+        else 
+        {
+            stack.push(s[i]);
+            let charCode = s[i].charCodeAt(0) - 97;
+            freq[charCode]++;
+        }
     }
-  
+    return stack.join('');
 
-    return  ans.filter((crr)=> crr != "*").join("");
-    
-   
 
+    // let stack = []
+    // for(let i=0; i<s.length; i++)
+    // {
+    //     if(s[i]==='*')
+    //     {
+    //         if(stack.length>0)
+    //         {
+    //             let minChar = stack.reduce((min, curr) => curr < min ? curr : min);
+    //             let minIndex = stack.lastIndexOf(minChar);
+    //             stack.splice(minIndex, 1);
+    //         }
+    //     }
+    //     else
+    //     {
+    //         stack.push(s[i])
+    //     }
+    // }   
+    // return stack.join('')
+
+
+    // This Exceeds the time limit but works
+    // let count = 0;
+    // for(let i=0; i<s.length; i++)
+    // {
+    //     if(s[i]==='*')
+    //         count++;
+    // }
+    // if(count===0)
+    //     return s;
+    // while(count>0)
+    // {
+    //     let stack = []
+    //     for(let i=0; i<s.length; i++)
+    //     {
+    //         let temp = s[i]
+    //         if(temp!=='*')
+    //         {
+    //             stack.push(temp)
+    //         }
+    //         else
+    //         {
+    //             let minChar = stack.reduce((min, curr) => curr < min ? curr : min);
+    //             let index = stack.lastIndexOf(minChar);
+    //             stack.splice(index, 1);
+    //             for(let j = i + 1; j < s.length; j++) 
+    //             {
+    //                 stack.push(s[j]);
+    //             }
+    //             break;
+    //         }    
+    //     }
+    //     s = stack.join('');
+    //     count--;
+    // }
+    // return s;
 };
