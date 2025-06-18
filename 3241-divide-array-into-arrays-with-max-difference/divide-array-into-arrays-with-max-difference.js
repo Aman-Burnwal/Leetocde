37bf73a9bcf48b60e449a639b58847e74608abcd
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-06-18 09:52:55 +0530

    Time: 87 ms (62.96%) | Memory: 85.2 MB (77.78%) - LeetSync

---
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[][]}
+ */
+var divideArray = function(nums, k) {
+    nums.sort((a, b) => a - b);
+
+    const ans = [];
+
+    for(let i = 0; i < nums.length; i+= 3) {
+        if(nums[i + 2] - nums[i] > k) return []
+        ans.push(nums.slice(i, i + 3));
+    }
+
+
+
+    return ans;
+};
