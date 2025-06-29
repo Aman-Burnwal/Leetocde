f844115dbcad0b85d30e6e07acb785c1476fe56f
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-06-29 18:59:33 +0530

    Time: 472 ms (6.10%) | Memory: 69.5 MB (25.61%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.js
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.js
@@ -6,6 +6,7 @@
 var numSubseq = function(nums, target) {
     nums.sort((a, b) => a - b);
     const MOD = Math.pow(10, 9) + 7;
+    const BMOD = BigInt(MOD)
     let count =  0;
 
 
@@ -17,8 +18,8 @@ var numSubseq = function(nums, target) {
             
             while(left <= right && nums[left] + nums[right] > target) right--;
 
-           if(left<= right) count = (BigInt(count) + (2n ** BigInt(right - left)) % BigInt(MOD)) %BigInt(MOD);
-           count = parseInt(count)
+           if(left<= right) count = ((count) + parseInt((2n ** BigInt(right - left)) % BMOD)) %(MOD);
+
            
            left++;
         }
