7354a173bc192a315f0a3768d9ae470b43780535
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-08-10 12:38:53 +0530

    Time: 652 ms (11.11%) | Memory: 61.6 MB (7.41%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var reorderedPowerOf2 = function(n) {
+    let t =( n & (n -1)) === 0;
+     console.log(t)
+    const str = String(n).split('').sort((a, b) => a - b);
+    permutation(str, [], []);
+    return t;
+
+
+
+    function permutation(nums, per_arr, temp) {
+        if(t)return;
+        if(temp.length && temp[0] == 0) return;
+
+        if(nums.length === 0) {
+            if(temp[0] == 0) return;
+            let num = Number(temp.join(''));
+            if((num & (num -1)) === 0) t = true;
+            return;
+        }
+
+        for(let i = 0; i < nums.length; i++) {
+            if(nums[i] === nums[i - 1]) continue;
+            let newTemp = [...temp]
+            newTemp.push(nums[i]);
+
+            let newNum = nums.slice(0, i).concat(nums.slice(i + 1))
+            permutation(newNum, per_arr, newTemp);
+        }
+    }
+};
