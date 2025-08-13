7519f918653507c7d7c038f968321fca7a00cbae
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-08-13 20:16:08 +0530

    Time: 23 ms (7.93%) | Memory: 65.3 MB (37.98%) - LeetSync

---
+++ 326-power-of-three/power-of-three.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    for(let i = 0; i <= 31; i++) {
+        if(Math.pow(3, i) === n) return true;
+    }
+    return false;
+};
+
