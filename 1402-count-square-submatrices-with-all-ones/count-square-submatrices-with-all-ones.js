73d35d302a8d5bf15d5dffaf181a04af2478d56e
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-08-21 17:55:03 +0530

    Time: 8 ms (82.19%) | Memory: 58.6 MB (76.71%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.js
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.js
@@ -5,15 +5,37 @@
 var countSquares = function(matrix) {
     const rowLen = matrix.length;
     const colLen = matrix[0].length;
-    let reuslt = 0;
-    const memo = Array.from({length: rowLen}, () => new Array(colLen).fill(-1))
-    for(let row = 0; row < rowLen; row++) {
-        for(let col = 0; col < colLen; col++) {
-            if(matrix[row][col] === 1) {
-                reuslt += helper(row, col);
+    // let reuslt = 0;
+    // const memo = Array.from({length: rowLen}, () => new Array(colLen).fill(-1))
+    // for(let row = 0; row < rowLen; row++) {
+    //     for(let col = 0; col < colLen; col++) {
+    //         if(matrix[row][col] === 1) {
+    //             reuslt += helper(row, col);
+    //         }
+    //     }
+    // }
+
+    let count = 0;
+
+    for(let i = rowLen - 2; i >= 0; i--) {
+        for(let j = colLen - 2; j >= 0; j--) {
+            if(matrix[i][j] === 1) {
+                let right = matrix[i][j + 1]
+                let digonal = matrix[i + 1][j + 1]
+                let down = matrix[i + 1][j]
+                matrix[i][j] += Math.min(right, digonal, down)
+                count += matrix[i][j];
+                
             }
         }
     }
+    for(let i = 0; i < rowLen; i++) {
+        count += matrix[i][colLen - 1];
+    }
+    for(let i = 0; i < colLen - 1; i++) {
+        count += matrix[rowLen - 1][i];
+    }
+    return count;
 
     return reuslt;
 
