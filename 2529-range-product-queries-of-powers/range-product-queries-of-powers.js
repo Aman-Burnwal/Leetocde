57e05dc2a30bd48eaecb684e8d2912b84685be71
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-08-24 16:09:56 +0530

    Time: 41 ms (67.29%) | Memory: 88.4 MB (58.02%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.js
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.js
@@ -4,20 +4,30 @@
  * @return {number[]}
  */
 var productQueries = function(n, queries) {
-    const powers = [];
-    let MOD = Math.pow(10, 9) + 7;
-    for(let i = 0; i < 31; i++) {
-        if(n & (1 << i)) powers.push(1 << i);
+    const pow = [];
+    for(let i = 0; i <= 31; i++) {
+        pow.push(Math.pow(2, i))
     }
 
-    const array = [1];
-    for(let i = 0; i < powers.length; i++) {
-        array.push(array[i] * powers[i]);
+    const powers = [];
+    for(let i = pow.length - 1; i >= 0; i--) {
+        if(pow[i] <= n) {
+            powers.push(pow[i]);
+            n-= pow[i]
+        }
+       
     }
+    powers.reverse();
+
+
+    const preComputed = [1];
+    powers.forEach((power, idx) => preComputed[idx + 1] = preComputed[idx] * power);
+
     const ans = [];
+    const mod = 1e9 + 7;
     for(const [start, end] of queries) {
-        let val = ((array[end + 1]) / (array[start]) ) % MOD
-        ans.push(val)
+        const val = preComputed[end + 1] / preComputed[start];
+        ans.push(val % mod)
     }
     return ans;
 };
