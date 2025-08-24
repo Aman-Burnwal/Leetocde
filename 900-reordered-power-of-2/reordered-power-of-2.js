b1af7d08c59f60cc84b55866ea51199a3e0970db
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-08-24 15:54:03 +0530

    Time: 4 ms (32.76%) | Memory: 56 MB (30.46%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.js
+++ 900-reordered-power-of-2/reordered-power-of-2.js
@@ -3,31 +3,14 @@
  * @return {boolean}
  */
 var reorderedPowerOf2 = function(n) {
-    let t =( n & (n -1)) === 0;
-    const str = String(n).split('').sort((a, b) => a - b);
-    permutation(str, []);
-    return t;
+    let num = 1;
+    let pow = 1;
+    n = String(n).split("").sort((a, b) => b - a).join("");
 
-
-
-    function permutation(nums, temp) {
-        if(t)return;
-        if(temp.length && temp[0] == 0) return;
-
-        if(nums.length === 0) {
-            if(temp[0] == 0) return;
-            let num = Number(temp.join(''));
-            if((num & (num -1)) === 0) t = true;
-            return;
-        }
-
-        for(let i = 0; i < nums.length; i++) {
-            if(nums[i] === nums[i - 1]) continue;
-            let newTemp = [...temp]
-            newTemp.push(nums[i]);
-
-            let newNum = nums.slice(0, i).concat(nums.slice(i + 1))
-            permutation(newNum, newTemp);
-        }
+    for(let i = 0; i <= 31; i++) {
+        pow = Math.pow(2, i);
+        let num = String(pow).split("").sort((a, b) => b - a).join("");
+        if(num === n) return true;
     }
+    return false;
 };
