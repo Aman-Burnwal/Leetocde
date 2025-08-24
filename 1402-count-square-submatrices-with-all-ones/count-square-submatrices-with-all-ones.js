72c270761bedea423e462bf492fb54deac1fe193
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-08-24 16:57:37 +0530

    Time: 10 ms (57.27%) | Memory: 58.5 MB (75.08%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.js
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.js
@@ -4,14 +4,15 @@
  */
 var countSquares = function(matrix) {
     let count = 0;
-    const dp = {};
-    for(let i = 0; i < matrix.length - 1; i++) {
-        for(let j = 0; j < matrix[0].length - 1; j++) {
-            if(matrix[i][j] === 1) count += helper(i, j)
 
+    for(let i = matrix.length -2; i >= 0;i--) {
+        for(let j = matrix[0].length -2; j >= 0; j--) {
+            if(matrix[i][j]) {
+                matrix[i][j] += Math.min(matrix[i + 1][j], matrix[i + 1][j + 1], matrix[i][j + 1])
+                count += matrix[i][j];
+            }
         }
     }
-    console.log(count)
 
     for(let i = 0; i < matrix.length; i++) {
         count += matrix[i][matrix[0].length - 1];
@@ -21,17 +22,5 @@ var countSquares = function(matrix) {
     }
     return count;
 
-    function helper(i , j) {
-        const key = i + "_"  + j;
-        if(dp.hasOwnProperty(key)) return dp[key]
-        if(i >= matrix.length) return 0;
-        if(j >= matrix[0].length) return 0;
-        if(matrix[i][j] === 0) return 0;
 
-        let right = helper(i, j + 1);
-        let diagonal = helper(i + 1, j + 1);
-        let down = helper(i + 1, j);
-
-        return dp[key] = 1 + Math.min(right, down, diagonal);
-    }
 };
