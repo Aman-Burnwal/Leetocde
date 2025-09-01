e1a26120b706589649fc2eefbafdfac2fdfdb824
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-01 14:43:59 +0530

    Time: 4 ms (90.00%) | Memory: 56.9 MB (84.50%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    const set = new Set();
-    let pre = 0;
-    let max = 0;
-
-    for(let i = 0; i < s.length; i++) {
-        if(set.has(s[i])) {
-            while(set.has(s[i])) {
-                set.delete(s[pre++])
-            }
-        }
-        set.add(s[i]);
-        max = Math.max(max, set.size)
-    }
-    return max;
+    let maxLen = 0;
+    let left = 0;
+    const memo = new Set()
+    for(let idx = 0; idx < s.length; idx++) {
+        const ch = s[idx];
+        while(memo.has(ch)) {
+            memo.delete(s[left++])
+        };
+        memo.add(ch);
+        maxLen = Math.max(maxLen, memo.size)
+    };
+    return maxLen;
 };
