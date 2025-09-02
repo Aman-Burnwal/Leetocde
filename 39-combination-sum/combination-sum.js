6fd07aed7f067d293bc0e8d773a69a7eead42328
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-02 18:17:40 +0530

    Time: 2 ms (84.07%) | Memory: 59.9 MB (51.71%) - LeetSync

--- 39-combination-sum/combination-sum.js
+++ 39-combination-sum/combination-sum.js
@@ -5,6 +5,7 @@
  */
 var combinationSum = function(candidates, target) {
     const result = [];
+    candidates.sort((a, b) => a - b);
     function helper(curr,temp, start ) {
         if(curr === target) {
             result.push([...temp]);
@@ -12,7 +13,7 @@ var combinationSum = function(candidates, target) {
         }
 
         for(let i = start; i < candidates.length; i++) {
-            if(curr + candidates[i] > target) continue;
+            if(curr + candidates[i] > target) break;
             temp.push(candidates[i]);
             helper(curr + candidates[i], temp , i);
             temp.pop();
