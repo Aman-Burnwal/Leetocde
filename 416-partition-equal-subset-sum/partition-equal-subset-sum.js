34a8a9ff15e97697b4f3e792db8dbbb8685b109c
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-02 23:47:27 +0530

    Time: 597 ms (13.06%) | Memory: 118.3 MB (11.75%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.js
@@ -3,30 +3,30 @@
  * @return {boolean}
  */
 var canPartition = function(nums) {
-
-
-    let sum = 0;
-
-    for(const num of nums) sum += num;
-
-    if( sum % 2 ) return false;
-    const half = sum / 2;
-    const dp = Array.from({length : nums.length}, () => new Array(half + 1));
-
-
     
+    let total = nums.reduce((acc, curr) => acc + curr, 0)
 
-    return subset( 0, 0 );
+    const dp = Array.from({length : nums.length}, () => new Array(total + 1));
+    total = total / 2;
+
+    if(total !== Math.floor(total)) return false;
+
+    return helper(0, 0);
+
+    function helper(i, sum) {
+        if(sum === total ) return true;
+        if(i >= nums.length || sum > total) {
+            return false;
+        }
+        
+
+        if(dp[i][sum] != undefined) return dp[i][sum]
+        
+        let take = helper(i + 1, sum + nums[i]);
+        let not = helper(i + 1, sum);
+        
+        return dp[i][sum] = take || not
 
 
-    function subset( i, sum ) {
-
-        if(sum == half ) return true;
-
-        if( i >= nums.length || sum > half ) return false;
-        if(dp[i][sum] !== undefined) return dp[i][sum];
-
-        return dp[i][sum] = ( subset( i + 1, sum + nums[i] ) ||  subset( i + 1, sum ) )
     }
-    
 };
