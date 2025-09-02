6f8ec87e4dc3ace791da4a86cd8c184da131cad8
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-02 17:48:00 +0530

    Time: 527 ms (40.52%) | Memory: 56.8 MB (57.24%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.js
+++ 37-sudoku-solver/sudoku-solver.js
@@ -3,22 +3,18 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solveSudoku = function(board) {
-
-    helper();
-    return board;
     
-    function helper() {
+    function solver() {
 
-        for(let i = 0; i < 9; i++) {
-            for(let j = 0; j < 9; j++) {
-                if(board[i][j] === ".") {
+        for(let row = 0; row < board.length; row++) {
+
+            for(let col = 0; col < board[0].length; col++) {
+                if(board[row][col] === ".") {
                     for(let num = 1; num < 10; num++) {
-                        const isSafe = isValid(i, j, num.toString());
-                        if(isSafe) {
-                            board[i][j] = num.toString();
-                            if(helper()) return true;
-                            board[i][j] = ".";
-                         
+                        if(canUse(num.toString(), row, col)) {
+                            board[row][col] = num.toString();
+                            if(solver()) return true;
+                            board[row][col] = ".";
                         }
                     }
                     return false;
@@ -29,22 +25,26 @@ var solveSudoku = function(board) {
         return true;
     }
 
-    function isValid (i, j, num) {
-        for(let row = 0; row < 9; row++) {
-            if(board[row][j] === num) return false;
+    function canUse (num, row, col) {
+        for(let cols = 0; cols < board[0].length; cols++) {
+            if(board[row][cols] === num) return false;
         }
-        for(let col = 0; col < 9; col++) {
-            if(board[i][col] === num) return false;
+        for(let rows = 0; rows < board[0].length; rows++) {
+            if(board[rows][col] === num) return false;
         }
-        const row = Math.floor(i / 3) * 3;
-        const col = Math.floor(j / 3) * 3;
-        
-        for(let row1 = row; row1 < row + 3; row1++) {
-            for(let col1 = col; col1 < col + 3; col1++) {
-                if(board[row1][col1] === num) return false;
+
+        const rows = Math.floor(row / 3) * 3;
+        const cols = Math.floor(col / 3) * 3;
+
+        for(let subRow = rows; subRow < rows + 3; subRow++) {
+            for(let subCol = cols; subCol < cols + 3; subCol++) {
+                if(board[subRow][subCol] === num) return false;
             }
         }
         return true;
-    
     }
+
+
+    solver()
+    return board;
 };
