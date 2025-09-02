3997c135711670e85907dd28e9408ad22d1fd010
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-02 17:33:34 +0530

    Time: 6 ms (5.74%) | Memory: 55.3 MB (20.15%) - LeetSync

---
+++ 31-next-permutation/next-permutation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+
+    for(let right = nums.length - 2; right >= 0; right--) {
+        for(let rightMost = nums.length - 1; rightMost > right; rightMost--) {
+            if(nums[rightMost] > nums[right]) {
+                [nums[rightMost], nums[right]] = [nums[right], nums[rightMost]];
+                const slice = nums.slice(right + 1).sort((a, b) => a - b);
+
+
+                for(let idx = right + 1; idx < nums.length; idx++) {
+                    nums[idx] = slice[idx - right - 1];
+                }
+            
+                return nums;
+            }
+        }
+    }
+    return nums.sort((a, b) => a - b)
+};
