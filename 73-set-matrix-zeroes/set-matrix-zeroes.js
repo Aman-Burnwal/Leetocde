fe136ce23ec091c2f7a4cd19c8391f927c66ff96
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-03 15:35:21 +0530

    Time: 4 ms (42.11%) | Memory: 58.5 MB (84.49%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -5,23 +5,25 @@
 var setZeroes = function(matrix) {
     let firstRowHasZero = false;
     let firstColHasZero = false;
+    const rowLength = matrix.length;
+    const colLength = matrix[0].length;
 
-    for(let row = 0; row < matrix.length; row++) {
+    for(let row = 0; row < rowLength; row++) {
         if(matrix[row][0] === 0) {
             firstColHasZero = true;
             break;
         }
     }
 
-    for(let col = 0; col < matrix[0].length; col++) {
+    for(let col = 0; col < colLength; col++) {
         if(matrix[0][col] === 0) {
             firstRowHasZero = true;
             break;
         }
     }
 
-    for(let row = 1; row < matrix.length; row++) {
-        for(let col = 1; col < matrix[0].length; col++) {
+    for(let row = 1; row < rowLength; row++) {
+        for(let col = 1; col < colLength; col++) {
             if(matrix[row][col] === 0) {
                 matrix[row][0] = 0;
                 matrix[0][col] = 0;
@@ -29,8 +31,8 @@ var setZeroes = function(matrix) {
         }
     }
 
-    for(let row = 1; row < matrix.length; row++) {
-        for(let col = 1; col < matrix[0].length; col++) {
+    for(let row = 1; row < rowLength; row++) {
+        for(let col = 1; col < colLength; col++) {
             if(matrix[row][0] === 0 || matrix[0][col] === 0) {
                 matrix[row][col] = 0;
             }
@@ -39,7 +41,7 @@ var setZeroes = function(matrix) {
 
     if(firstColHasZero) {
 
-        for(let row = 0; row < matrix.length; row++) {
+        for(let row = 0; row < rowLength; row++) {
             matrix[row][0] = 0
         
         }
@@ -49,7 +51,7 @@ var setZeroes = function(matrix) {
 
     if(firstRowHasZero) {
 
-        for(let col = 0; col < matrix[0].length; col++) {
+        for(let col = 0; col < colLength; col++) {
             matrix[0][col] = 0
         
         }
