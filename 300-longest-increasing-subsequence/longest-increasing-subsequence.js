274754b196355a340ac675d090e1b89d6a8dbb5e
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-03 10:36:26 +0530

    Time: 78 ms (61.81%) | Memory: 56.1 MB (48.36%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.js
@@ -3,16 +3,13 @@
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
-    const dp = Array.from({length: nums.length}, () => new Array(nums.length))
-    const ans = helper(0, -1);
-    function helper(idx, prevIdx) {
-        if(idx >= nums.length) return 0;
-        if(dp[idx][prevIdx] != undefined) return dp[idx][prevIdx];
-        const skip = helper(idx + 1, prevIdx);
-        let take = 0;
-        if(prevIdx === -1 || nums[prevIdx] < nums[idx]) take = 1 + helper(idx + 1, idx);
-        // console.log(take, skip)
-        return dp[idx][prevIdx] = Math.max(take, skip) 
+    const dp = new Array(nums.length).fill(1)
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i + 1; j < nums.length; j++) {
+            if(nums[j] > nums[i]) {
+                dp[j] = Math.max(dp[j], dp[i] + 1)
+            }
+        }
     }
-    return ans;
+    return Math.max(...dp);
 };
