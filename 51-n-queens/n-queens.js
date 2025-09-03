76710e89b70d6c17e27d6376bee0a1235271d426
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-03 14:35:16 +0530

    Time: 967 ms (5.02%) | Memory: 64.8 MB (5.28%) - LeetSync

--- 51-n-queens/n-queens.js
+++ 51-n-queens/n-queens.js
@@ -3,76 +3,58 @@
  * @return {string[][]}
  */
 var solveNQueens = function(n) {
-
     
-    const chess = Array.from({length : n}, () => new Array(n).fill("."))
-    const ans = new Array();
-
-    helper( 0);
+    const queen = Array.from({length: n}, () => new Array(n).fill("."))
+    const ans = []
 
 
+    function backtrack(row, col, count) {
+        if(row >= n) {
+            const board = queen.map((row) => row.join(""));
+            if(count ===  n) ans.push(board)
+        }
+        // console.log(row)
+        for(let i = row; i < n; i++) {
+            for(let j = col; j < n; j++) {
+                // console.log(queen[i][j], isSafe(i, j))
+                if( queen[i][j] === "." && isSafe(i , j)) {
+                    queen[i][j] = "Q";
+                    backtrack(i + 1, 0, count + 1);
+                    queen[i][j] = "."
+                }
+            }
+        }
+    }
+    backtrack(0, 0, 0)
     return ans;
 
 
 
-    function helper( col) {
-    
-        // console.log(col, row , col >= n)
-        if(col >= n) {
-
-            let temp = [];
-
-            for(const rows of chess) {
-                temp.push(rows.join(""))
-            }
-            ans.push([...temp])
-            return;
-        }
-
-        for(let i = 0; i < n; i++) {
-
-            if(isSafe(i, col)) {
-
-                chess[i][col] = "Q";
-
-                helper( col + 1);
-
-                chess[i][col] = "."
-            }
-        }
-        
- 
-    }
-
-
     function isSafe(i, j) {
-       
-        
-        for (let row = i - 1; row >= 0; row--) {
-            if (chess[row][j] == "Q") return false;
+
+        for(let row = 0; row < n; row++) {
+            if(queen[row][j] === 'Q' || queen[i][row] === "Q") return false;
         }
 
-        
-        for (let col = j - 1; col >= 0; col--) {
+        let row = i, col = j;
 
-            if (chess[i][col] == "Q") return false;
-
+        while(row >= 0 && col >= 0) {
+            if(queen[row--][col--] === 'Q') return false;
+        }
+        row = i, col = j;
+        while(row < n && col < n) {
+            if(queen[row++][col++] === 'Q') return false;
         }
 
- 
-        for (let left = 1; i - left >= 0 && j - left >= 0; left++) {
-            if (chess[i - left][j - left] == "Q") return false;
+        row = i, col = j;
+        while(row >= 0 && col < n) {
+            if(queen[row--][col++] === 'Q') return false;
         }
 
-
-
-        for (let right = 1; i + right < n && j - right >= 0; right++) {
-            if (chess[i + right][j - right] == "Q") return false;
+        row = i, col = j;
+        while(row < n && col >= 0) {
+            if(queen[row++][col--] === 'Q') return false;
         }
-
-
         return true;
     }
-
-    
-};
+}; 
