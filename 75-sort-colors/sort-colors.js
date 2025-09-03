92d00531362b6ed98b14a552fa66814510f8c2bc
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-03 15:47:20 +0530

    Time: 1 ms (40.48%) | Memory: 53.6 MB (70.24%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -6,31 +6,20 @@ var sortColors = function(nums) {
     let left = 0;
     let right = nums.length - 1;
 
-    // for(let i = 0; i < right; i++) {
-    //     if(nums[i] === 0) {
-    //         [nums[i], nums[left]] = [nums[left], nums[i]];
-    //         left++;
+    for(let i = 0; i <= right; i++) {
+        if(nums[i] === 0) {
+            [nums[i], nums[left]] = [nums[left], nums[i]];
+            left++;
 
-    //     }
-    //     else if (nums[i] === 2) {
-    //         [nums[i], nums[right]] = [nums[right], nums[i]];
-    //         i--;
-    //         right--;
-    //     }
-    // }
-
-    let zero = 0;
-    let one = 0;
-    let two = 0;
-    
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] === 0) zero++;
-        else if(nums[i] === 1) one++;
-        else two++;
+        }
+        else if (nums[i] === 2) {
+            [nums[i], nums[right]] = [nums[right], nums[i]];
+            i--;
+            right--;
+        }
     }
 
-    let i  = 0;
-    while(zero--) nums[i++] = 0;
-    while(one--) nums[i++] = 1;
-    while(two--) nums[i++] = 2;
+
+    
+
 };
