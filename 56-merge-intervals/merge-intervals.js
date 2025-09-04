9cafba24b08c226855fde977511d246d8515f7ab
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-04 14:10:40 +0530

    Time: 10 ms (46.77%) | Memory: 65.2 MB (43.33%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -3,20 +3,17 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0])
+    intervals.push([-1, -1])
+
+    const ans = [intervals[0]]
+
     
-    intervals.sort((a, b) => a[0] - b[0]);
-
-    const ans = new Array();
-
-    ans.push(intervals[0]);
-
-    for(let i = 1; i < intervals.length; i++) {
-
-        if(intervals[i][0] <= ans[ans.length - 1][1] ) {
-
-            ans[ans.length - 1][1] = Math.max(ans[ans.length - 1][1], intervals[i][1]);
+    for(let i = 1; i < intervals.length - 1; i++) {
+        let last = ans[ans.length -1][1];
+        if(intervals[i][0] <= last) {
+            ans[ans.length - 1][1] = Math.max(last, intervals[i][1]);
         }
-
         else ans.push(intervals[i]);
     }
 
