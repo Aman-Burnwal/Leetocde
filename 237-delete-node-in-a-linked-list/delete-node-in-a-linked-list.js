abd9473acc8e0cd59d9c1d491e5311a4b10cb350
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-04 09:51:27 +0530

    Time: 55 ms (29.97%) | Memory: 58.3 MB (13.19%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    const temp = node.next.next;
+    node.next = null;
+    node.next = temp;
+
+};
