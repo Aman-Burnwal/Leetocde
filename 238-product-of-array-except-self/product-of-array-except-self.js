82974c4a10d230765b820937ad8c9ec17fc51f51
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-04 14:28:13 +0530

    Time: 10 ms (39.77%) | Memory: 69.6 MB (73.46%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -4,31 +4,23 @@
  */
 var productExceptSelf = function(nums) {
 
-
-    let mul = 1;
+    let totalMul = 1;
     let zero = 0;
-
-
-    for(const num of nums) {
-
-        if(num == 0) zero++;
-        else mul *= num;
-    }
-
-  
-
     for(let i = 0; i < nums.length; i++) {
-
-        if(nums[i] == 0 && zero > 1) nums[i] = 0;
-        else if(nums[i] == 0 && zero == 1) nums[i] = mul;
-        else if(nums[i] != 0 && zero) nums[i] = 0;
-        else nums[i] = mul / nums[i]
-        
-
+        if(nums[i] === 0) {
+            zero++;
+            if(zero === 2) {
+                totalMul = 0;
+                break;
+            }
+        } else totalMul *= nums[i]
     }
-    
-    return nums;
-
-
-    
+    const ans = []
+    for(let i = 0; i < nums.length; i++) {
+        if(totalMul === 0 && zero === 2) ans[i] = 0;
+        else if (zero === 1 && nums[i] === 0) ans[i] = totalMul;
+        else if (zero === 1) ans[i] = 0;
+        else ans[i] = totalMul / nums[i]; 
+    }
+    return ans;
 };
