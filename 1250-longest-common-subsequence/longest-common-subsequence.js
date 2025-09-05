c2cdb8d1e69f2a35e000242260e8bf3fae26af75
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-05 12:04:55 +0530

    Time: 78 ms (24.27%) | Memory: 79.1 MB (82.13%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.js
+++ 1250-longest-common-subsequence/longest-common-subsequence.js
@@ -3,17 +3,15 @@
  * @param {string} text2
  * @return {number}
  */
-var longestCommonSubsequence = function(A, B) {
-    // const dp = Array.from({length: text1.length + 1}, () => new Array(text2.length))
-    const Memo = Array.from({length: A.length}, () => new Array(B.length).fill(-1));
-    const R = (n, m) => {
-        if (n < 0 || m < 0) return 0;
-        if (Memo[n][m] != -1) return Memo[n][m]; //add
+var longestCommonSubsequence = function(text1, text2) {
+    const dp = Array.from({length: text1.length + 1}, () => new Array(text2.length + 1))
+    return rec(0, 0);
+    function rec (i, j) {
+        if(i >= text1.length || j >= text2.length) return 0;
+  
+        if(dp[i][j] != undefined) return dp[i][j]
+        if(text1[i] === text2[j]) return dp[i][j] = rec(i + 1, j + 1) + 1;
+        return dp[i][j] = Math.max(rec(i + 1, j), rec(i, j+ 1));
 
-        if (A[n] == B[m]) Memo[n][m] = R(n - 1, m - 1) + 1;
-        else Memo[n][m] = Math.max(R(n, m - 1), R(n - 1, m));
-
-        return Memo[n][m]; // add Memo to memoize the results
-    };
-    return R(A.length - 1, B.length - 1);
+    }
 };
