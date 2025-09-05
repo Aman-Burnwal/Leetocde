d6be44e94ef928b445df25c7428891a47f04cb3c
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-05 09:47:06 +0530

    Time: 14 ms (59.42%) | Memory: 62.8 MB (49.62%) - LeetSync

--- 207-course-schedule/course-schedule.js
+++ 207-course-schedule/course-schedule.js
@@ -5,32 +5,29 @@
  */
 var canFinish = function(numCourses, prerequisites) {
     const map = new Map();
-    const inDegree = new Array(numCourses).fill(0);
+    const inOrder = new Array(numCourses).fill(0);
 
-    for(const [course, preReq] of prerequisites) {
+    for(const [course, preReq] of prerequisites ) {
         if(!map.has(course)) map.set(course, []);
         map.get(course).push(preReq);
-        inDegree[preReq]++;
-    }
-
-
-    const queue = [];
-
-    for(let i = 0; i < inDegree.length; i++) {
-        if(inDegree[i] === 0) queue.push(i)
+        inOrder[preReq]++;
     }
 
     let count = 0;
+    const queue = []
+    for(let i = 0; i < numCourses; i++) {
+        if(inOrder[i] === 0) queue.push(i);
+    }
+
     while(queue.length) {
-        count++;
         const course = queue.shift();
-        const req = map.get(course) || [];
-        for(const r of req) {
-            inDegree[r]--;
-            if(inDegree[r] === 0) queue.push(r)
+        count++;
+        const d = map.get(course) || []
+        for(const preReq of d) {
+            inOrder[preReq]--;
+            if(inOrder[preReq] === 0) queue.push(preReq);
         }
     }
+
     return count === numCourses;
-
-
 };
