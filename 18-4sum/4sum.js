edbc55b20cdf7293b59d8687059913dc46c410b0
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-05 11:21:41 +0530

    Time: 14 ms (82.88%) | Memory: 57.5 MB (48.57%) - LeetSync

---
+++ 18-4sum/4sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    nums.sort((a, b) => a - b);
+    const ans = [];
+    for(let i = 0; i < nums.length; i++) {
+        if(i > 0 && nums[i] === nums[i - 1]) continue;
+        for(let j = i + 1; j < nums.length; j++) {
+            if(j > (i + 1) && nums[j] === nums[j - 1]) continue;
+            let left = j + 1; 
+            let sum = nums[i] + nums[j];
+            let right = nums.length - 1;
+            while(left < right) {
+                const totalSum = sum + nums[left] + nums[right];
+                if(totalSum === target) {
+                    ans.push([nums[i], nums[j],nums[left], nums[right]]);
+                    right--;
+                    left++;
+                    while(left < right && nums[left] === nums[left - 1]) left++;
+                    while(right > left && nums[right] === nums[right + 1]) right--;
+                }
+                else if (totalSum > target) right--;
+                else left++;
+                
+                
+            }
+        }
+    } 
+    return ans;
+};
