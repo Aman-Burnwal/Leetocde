c78120bcd5ccbce47623bc08418ec45fd6219c73
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-06 11:25:52 +0530

    Time: 64 ms (72.53%) | Memory: 87 MB (65.21%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.js
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -4,28 +4,17 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function(nums, k) {
-    const deque = []; // will store indices of useful elements in decreasing order
+    const deque = []; 
     const result = [];
 
-    for (let i = 0; i < nums.length; i++) {
-        // Remove elements out of window (left side)
-        if (deque.length && deque[0] <= i - k) {
-            deque.shift();
-        }
-
-        // Maintain decreasing order in deque
-        while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
-            deque.pop();
-        }
-
-        // Add current element index
+    for(let i = 0; i < nums.length; i++) {
+        
+        while(deque.length && nums[deque[deque.length - 1]] <= nums[i]) deque.pop();
         deque.push(i);
 
-        // Record maximum once we hit window size
-        if (i >= k - 1) {
-            result.push(nums[deque[0]]);
-        }
+        if(deque[0] <= i - k) deque.shift();
+        
+        if(i >= k - 1) result.push(nums[deque[0]]);
     }
-
     return result;
 };
