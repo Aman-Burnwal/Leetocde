f66603bc5544c7d39357fbf6695c59957a827bf0
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-08 21:16:15 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (13.03%) - LeetSync

--- 198-house-robber/house-robber.js
+++ 198-house-robber/house-robber.js
@@ -3,15 +3,16 @@
  * @return {number}
  */
 var rob = function(nums) {
-    const dp = new Array(nums.length).fill(-1)
-    return helper(0)
+    let n = nums.length;
+    if (n === 1) return nums[0];
 
-    function helper(i) {
+    const dp = new Array(n).fill(0);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
 
-        if(i >= nums.length) return 0;
-        if(dp[i] != -1) return dp[i]
-        return dp[i] = Math.max(helper(i + 1) , nums[i] + helper(i + 2))
-
-
+    for(let i = 2; i < n; i++) {
+        dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1])
     }
+
+    return dp[n - 1];
 };
