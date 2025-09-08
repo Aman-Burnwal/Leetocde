245d32e21d5e1aaef63b447b7e8699078cc157a2
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-08 17:21:14 +0530

    Time: 1 ms (78.13%) | Memory: 56.8 MB (11.05%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -4,15 +4,17 @@
  */
 var minCostClimbingStairs = function(cost) {
 
-    cost.push(0)
-    const dp = new Array(cost.length).fill(0);
     let len = cost.length - 1;
-    cost.push(0, 0)
+    let first = 0;
+    let second = 0
 
     for(let i = len; i >= 0; i--) {
-        cost[i] += Math.min(cost[i + 2], cost[i + 1])
+        const curr = cost[i] +  Math.min(first, second)
+        second = first;
+        first = curr;
+    
     }
-    return Math.min(cost[0], cost[1])
+    return Math.min(first, second)
 
 
 
