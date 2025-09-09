47cee90df3e04758ecade45c4e4e3006bdb7e0ae
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-09 15:24:44 +0530

    Time: 10 ms (66.70%) | Memory: 62 MB (49.69%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.js
+++ 210-course-schedule-ii/course-schedule-ii.js
@@ -3,45 +3,41 @@
  * @param {number[][]} prerequisites
  * @return {number[]}
  */
-var findOrder = function(n, prerequisites) {
+var findOrder = function(numCourses, prerequisites) {
     
-    const map = new Map();
-    const indegree = new Array(n).fill(0);
+    const graph = new Map();
+    const depend = new Array(numCourses).fill(0);
 
-    for(const [v, u] of prerequisites) {
-
-        if(!map.has(u))map.set(u, []);
-
-        map.get(u).push(v);
-        indegree[v]++
+    for(const [course, req] of prerequisites) {
+        if(!graph.has(req)) graph.set(req, []);
+        graph.get(req).push(course);
+        depend[course]++;
     }
 
-    const que = new Array();
-    const ans = new Array()
+    const queue = [];
 
-    for(let i = 0; i < n; i++) {
-
-        if(indegree[i] == 0) que.push(i);
-        
-    }
-
-    while(que.length) {
-
-        const u = que.pop();
-        ans.push(u);
-
-        if(map.has(u))  {
-            for(const v of map.get(u)) {
-                indegree[v]--;
-                if(indegree[v] == 0) que.push(v);
-            }
+    let ans = [];
+    for(let i = 0; i < numCourses; i++) {
+        if(depend[i] === 0) {
+            queue.push(i)
         }
     }
 
-    return ans.length == n ? ans : [];
+    while(queue.length) {
 
+        const course = queue.shift();
+        ans.push(course);
 
+        const req = graph.get(course) || [];
 
+        for(const c of req) {
+            depend[c]--;
+            if(depend[c] === 0) {
+                queue.push(c);
+            }
+        }
 
+    }
 
+    return ans.length === numCourses ? ans : [];
 };
