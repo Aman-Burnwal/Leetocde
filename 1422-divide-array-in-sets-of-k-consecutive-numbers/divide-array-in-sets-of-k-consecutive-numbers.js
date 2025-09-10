1972b8b0c3b21a788c4a46f82b26be2f230407af
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-10 10:47:07 +0530

    Time: 102 ms (66.67%) | Memory: 82 MB (24.24%) - LeetSync

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var isPossibleDivide = function(nums, k) {
+    
+    const freq = new Map();
+
+    for(let num of nums) {
+        freq.set(num, (freq.get(num) || 0) + 1);
+    }
+
+    let keys = [...freq.keys()]
+    keys.sort((a, b) => a - b);
+
+    for(let i = 0; i < keys.length; i++) {
+
+        let count = freq.get(keys[i]);
+        if(count === 0) {
+            freq.delete(keys[i]);
+            continue;
+        }
+    
+            
+        for(let j = 0; j < k; j++) {
+            if(!freq.has(keys[i] + j)) return false;
+            if(freq.get(keys[i] + j) < count) return false;
+            freq.set(keys[i] + j, freq.get(keys[i] + j) - count)
+        }
+
+
+
+    }
+
+    return true;
+};
