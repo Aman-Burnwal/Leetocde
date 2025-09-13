46883cec665918b7f08631abb7ebf9388570aa02
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-13 18:11:51 +0530

    Time: 78 ms (87.35%) | Memory: 78.6 MB (92.01%) - LeetSync

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    points.sort((a, b) => a[1] - b[1]);
+    let end = points[0][1];
+    let arrow = 1;
+
+    for(let i = 1; i < points.length; i++) {
+        
+        if(points[i][0] > end) {
+            arrow++;
+            end = points[i][1];
+        }
+    }
+    return arrow;
+};
