2489d07aa4815c44205706c53b8e3b3647591514
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-13 14:38:33 +0530

    Time: 2 ms (41.53%) | Memory: 55.7 MB (38.87%) - LeetSync

--- 289-game-of-life/game-of-life.js
+++ 289-game-of-life/game-of-life.js
@@ -3,26 +3,24 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var gameOfLife = function(board) {
+
+    const dir = [[1, 1], [1, 0], [0, 1], [-1, -1], [-1, 0], [0, -1], [1, -1], [-1, 1]];
     const rowLen = board.length;
     const colLen = board[0].length;
+
     for(let row = 0; row < rowLen; row++) {
         for(let col = 0; col < colLen; col++) {
             const count = check(row, col)
             if(board[row][col] === 0 && count === 3) {
                 board[row][col] = 2;
             }
-        }
-    }
-
-    for(let row = 0; row < rowLen; row++) {
-        for(let col = 0; col < colLen; col++) {
-            if(board[row][col] === 1) {
-                const count = check(row, col)
-                if(count === 2 || count === 3) continue;
-                board[row][col] = -1
+            else if(board[row][col] === 1 && (count <= 1 || count > 3)) {
+                board[row][col] = -1;
             }
         }
     }
+
+
     for(let row = 0; row < rowLen; row++) {
         for(let col = 0; col < colLen; col++) {
             if(board[row][col] === 2) {
@@ -33,17 +31,17 @@ var gameOfLife = function(board) {
     }
 
 
+
     function check(row, col) {
         let count = 0;
-        if( row > 0 && (board[row - 1][col] === 1 || board[row - 1][col] === -1) ) count++;
-        if( col > 0 && (board[row][col - 1] === 1 || board[row][col - 1] === -1) ) count++;
-        if(row + 1 < rowLen && (board[row+ 1][col] === 1 || board[row+ 1][col] === -1) ) count++;
-        if(col + 1 < colLen && (board[row][col + 1] === 1 || board[row][col + 1] === -1) ) count++;
-        if(col + 1 < colLen && row + 1 < rowLen && (board[row + 1][col + 1] === 1 || board[row + 1][col + 1] === -1) ) count++;
-        if(row > 0 && col > 0 && (board[row - 1][col - 1] === 1 || board[row - 1][col - 1] === -1)) count++;
-        if(row > 0 && col + 1 < colLen &&( board[row - 1][col + 1] === 1 ||  board[row - 1][col + 1] === -1)) count++;
-        if(col > 0 && row + 1 < rowLen && (board[row + 1][col - 1] === 1 || board[row + 1][col - 1] === -1)) count++;
+        for(const [dx, dy] of dir ) {
+            let r1 = dx + row;
+            let c1 = dy + col;
 
+            if(r1 >= 0 && r1 < rowLen && c1 < colLen && c1 >= 0 && 
+            (board[r1][c1] === 1 || board[r1][c1] === -1)) count++
+        }
+        
         return count;
 
     }
