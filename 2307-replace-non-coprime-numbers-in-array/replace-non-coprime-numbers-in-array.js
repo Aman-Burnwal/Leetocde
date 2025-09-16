61562e0d408420905b314f343c5b5ee5f0755739
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-16 10:36:17 +0530

    Time: 35 ms (80.00%) | Memory: 83.6 MB (20.00%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.js
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.js
@@ -6,40 +6,34 @@ var replaceNonCoprimes = function (nums) {
     const ans = [nums[0]];
 
     for (let i = 1; i < nums.length; i++) {
+        let curr = nums[i];
+        let gcd = getGCD(curr, ans[ans.length - 1]);
 
-        const gcd = isNotPrime(nums[i], ans[ans.length - 1]);
         if (gcd > 1) {
-            const last = ans.pop()
-            let LCM = lc(nums[i], last, gcd);
-            
-            while(ans.length) {
-                const gcd = isNotPrime(ans[ans.length - 1], LCM);
-                if(gcd > 1) {
-                    LCM = lc(ans.pop(), LCM, gcd);
-                }
-                else break;
+            let lcm = getLCM(curr, ans.pop(), gcd);
+
+
+            while (ans.length) {
+                gcd = getGCD(ans[ans.length - 1], lcm);
+                if (gcd > 1) {
+                    lcm = getLCM(ans.pop(), lcm, gcd);
+                } else break;
             }
-            ans.push(LCM)
-        }
-
-        else {
-            ans.push(nums[i])
-
+            ans.push(lcm);
+        } else {
+            ans.push(curr);
         }
     }
-
 
     return ans;
-
-
-    function isNotPrime(num1, num2) {
-        if (num2 === 0) return num1;
-        return isNotPrime(num2, num1 % num2)
-    }
-
-    function lc(num1, num2, gcd) {
-        return (num1 * num2) / gcd;
-    }
-
-
 };
+
+
+function getGCD(a, b) {
+    if (b === 0) return a;
+    return getGCD(b, a % b);
+}
+
+function getLCM(a, b, gcd) {
+    return (a / gcd) * b;
+}
