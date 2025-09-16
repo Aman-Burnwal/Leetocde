1ff4d424b4b7a454c10c7b0d985ebe373c47d837
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2025-09-16 20:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 56.8 MB (91.01%) - LeetSync

---
+++ 112-path-sum/path-sum.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    if(!root) return false;
+    return dfs(root, targetSum)
+    function dfs(node, targetSum) {
+        if(!node) return false;
+        targetSum = targetSum - node.val;
+        if(!node.left && !node.right) {
+            return targetSum === 0;
+        }
+        
+        return dfs(node.left, targetSum) || dfs(node.right, targetSum)
+    }
+};
