409f4214c140595a05580d98af48dd91b08c9148
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2026-01-22 16:57:15 +0530

    Time: 1 ms (87.11%) | Memory: 55.7 MB (42.56%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,14 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    
     const map = new Map();
-    const numsLength = nums.length;
-    for(let idx = 0; idx < numsLength; idx++) {
-        const req = target - nums[idx];
-        if(map.has(req)) {
-            return [map.get(req), idx]
-        }
-        map.set(nums[idx], idx);
+
+    for(let i = 0; i < nums.length; i++) {
+        const num = target - nums[i];
+
+        if(map.has(num)) return [i, map.get(num)]
+        map.set(nums[i], i)
     }
-    return [0, 0];
 };
