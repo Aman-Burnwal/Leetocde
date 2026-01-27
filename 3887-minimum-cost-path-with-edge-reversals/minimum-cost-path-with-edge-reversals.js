27888767ebb3912d544af8bfe3d3020331610ed2
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2026-01-27 21:13:49 +0530

    Time: 699 ms (69.23%) | Memory: 130.4 MB (30.77%) - LeetSync

---
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.js
@@ -0,0 +1,42 @@
+// const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var minCost = function (n, edges) {
+    const graph = new Map();
+
+    for (const [u, v, w] of edges) {
+        if (!graph.has(u)) graph.set(u, []);
+        if (!graph.has(v)) graph.set(v, []);
+
+        graph.get(u).push([v, w]);
+        graph.get(v).push([u, 2 * w]);
+    }
+
+    const dist = new Array(n).fill(Infinity);
+    dist[0] = 0;
+
+    const pq = new PriorityQueue((x, y) => x[1] - y[1]);
+    pq.enqueue([0, 0]); 
+    while (!pq.isEmpty()) {
+        const [node, cost] = pq.dequeue();
+
+        if (node === n - 1) return cost;
+        if (cost > dist[node]) continue;
+
+        if (!graph.has(node)) continue;
+
+        for (const [next, w] of graph.get(node)) {
+            const newCost = cost + w;
+            if (newCost < dist[next]) {
+                dist[next] = newCost;
+                pq.enqueue([next, newCost]);
+            }
+        }
+    }
+
+    return -1;
+};
