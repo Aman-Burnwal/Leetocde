513fc9eaef21edb5df833dde2a4c46b39a67fdcd
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2026-03-04 21:11:21 +0530

    Time: 3 ms (65.71%) | Memory: 56.4 MB (51.43%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.js
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.js
@@ -5,34 +5,27 @@
 var numSpecial = function(mat) {
     const rows = new Array(mat.length).fill(0);
     const cols = new Array(mat[0].length).fill(0);
-
-    // let len = Math.min(mat.length, mat[0].length)
     let count = 0;
+
 
     for(let row = 0; row < mat.length; row++) {
         for(let col = 0; col < mat[0].length; col++) {
-            if(mat[row][col] === 1 && check(row, col)){
+            if(mat[row][col] === 1){
+                rows[row]++;
+                cols[col]++;
+            }
+        }
+    }
+
+  
+    for(let row = 0; row < mat.length; row++) {
+        for(let col = 0; col < mat[0].length; col++) {
+            if(mat[row][col] === 1 && rows[row] === 1 && cols[col] === 1){
                 count++
             }
         }
     }
 
-
-    function check(row, col) {
-        
-        for(let i = 0; i < mat.length; i++) {
-            if(i === row) continue;
-            if(mat[i][col]) return false;
-        }
-
-        for(let j = 0; j < mat[0].length; j++) {
-            if(j === col) continue;
-            if(mat[row][j]) return false;
-        }
-        return true;
-    }
-  
-   
     return count
 
     
