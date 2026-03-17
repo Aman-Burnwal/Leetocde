cfc9fd97bb6901f618b14a748e00588616ff19ec
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2026-03-17 21:10:05 +0530

    Time: 53 ms (45.45%) | Memory: 78.3 MB (54.55%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.js
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.js
@@ -6,27 +6,32 @@ var largestSubmatrix = function(matrix) {
     
     let n = matrix.length;
     let m = matrix[0].length;
+    let max = 0;
 
-    for(let j = 0; j < m; j++) {
-        let sum = 0;
-        for(let i = 0; i < n; i++) {
-            if(matrix[i][j] === 0) sum = 0;
-            else {
-                sum += 1;
-                matrix[i][j] = sum;
-            }
+    let dummy = new Array(m).fill(0)
+
+
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < m; j++) {
+            if(matrix[i][j] === 0) dummy[j] = 0;
+            else dummy[j]++;
         }
+        max = Math.max(max, maxValue())
+
+        // console.log(max)
+        // console.log(dummy)
     }
 
-    matrix.map(row => row.sort((b , a) => a - b));
-
-    let max = 0;
-    for(let row of matrix) {
+    function maxValue() {
+        let max = 0;
+        const row = [...dummy].sort((a, b) => b - a)
 
         for(let j = 0; j < m; j++) {
             if(row[j] === 0) break;
             max = Math.max(max, row[j]* (j + 1))
         }
+        return max;
     }
-    return max;
+
+    return max
 };
