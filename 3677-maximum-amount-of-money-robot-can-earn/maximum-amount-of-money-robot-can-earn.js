bf8f1ccd7ab310f09fcbc87b27a6c7569e68cb9b
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2026-04-02 18:36:34 +0530

    Time: 1444 ms (16.67%) | Memory: 112.8 MB (33.33%) - LeetSync

---
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.js
@@ -0,0 +1,57 @@
+/**
+ * @param {number[][]} coins
+ * @return {number}
+ */
+var maximumAmount = function(coins) {
+
+  let n = coins.length; 
+  let m = coins[0].length;
+
+
+  const map = Array.from({ length: n }, () =>
+        Array.from({ length: m }, () => Array(4).fill(null))
+    );
+    
+    return dfs(0, 0, 2) ;
+
+  function dfs(i, j , left) {
+    if(isSafeNot(i, j)) return -Infinity
+
+    
+
+
+    if(i === n - 1 && j === m - 1) {
+       
+        if(coins[i][j] < 0 && left > 0) return 0;
+        return coins[i][j];
+
+    }
+
+    let max = -Infinity;
+    if(map[i][j][left] != null) return map[i][j][left]
+
+
+    if(coins[i][j] < 0 && left > 0) {
+        let used = Math.max(dfs(i, j + 1, left - 1), dfs(i + 1, j, left - 1));
+        let unUsed = coins[i][j] + Math.max(dfs(i, j + 1, left), dfs(i + 1, j, left));
+
+        max = Math.max(used, unUsed);
+
+    }
+    else if(coins[i][j] < 0) {
+        max = coins[i][j] + Math.max(dfs(i, j + 1, left), dfs(i + 1, j, left));
+    }
+    else {
+        max = coins[i][j] +  Math.max(dfs(i, j + 1, left), dfs(i + 1, j, left));
+    }
+
+    map[i][j][left] = max;
+    return max;
+  } 
+
+
+  function isSafeNot(i, j) {
+    if(i >= n || j >= m) return true;
+    return false;
+  } 
+};
