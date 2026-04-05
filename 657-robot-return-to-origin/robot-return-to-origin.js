5c2c49142166790a6a7146f0f3f27145842e3f75
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2026-04-05 16:31:02 +0530

    Time: 7 ms (47.83%) | Memory: 57.2 MB (29.10%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    
+    const dir = [0, 0, 0, 0];
+
+    moves.split("").forEach((ch) => {
+        if(ch === "L") dir[0]++;
+        else if (ch === "R") dir[1]--;
+        else if (ch ===  "U") dir[2]++;
+        else dir[3]--
+    })
+
+    return ((dir[0] + dir[1]) === 0) &&  ((dir[2] + dir[3]) === 0)
+};
