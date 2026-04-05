e81496e8d04d35e938448a96d2feea00618e9ff7
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2026-04-05 16:31:30 +0530

    Time: 11 ms (13.38%) | Memory: 56.8 MB (32.44%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.js
+++ 657-robot-return-to-origin/robot-return-to-origin.js
@@ -6,12 +6,12 @@ var judgeCircle = function(moves) {
     
     const dir = [0, 0, 0, 0];
 
-    moves.split("").forEach((ch) => {
+    for(const ch of moves) {
         if(ch === "L") dir[0]++;
         else if (ch === "R") dir[1]--;
         else if (ch ===  "U") dir[2]++;
         else dir[3]--
-    })
+    }
 
     return ((dir[0] + dir[1]) === 0) &&  ((dir[2] + dir[3]) === 0)
 };
