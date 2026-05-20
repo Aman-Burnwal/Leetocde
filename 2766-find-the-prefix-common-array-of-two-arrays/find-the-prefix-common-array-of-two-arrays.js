b2a7079686ae01537306d668b9fc39f2bcf421ef
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2026-05-20 21:09:04 +0530

    Time: 7 ms (35.29%) | Memory: 61.1 MB (54.90%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.js
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.js
@@ -4,25 +4,20 @@
  * @return {number[]}
  */
 var findThePrefixCommonArray = function(A, B) {
-
-    const c = new Set()
-    const a = new Set();
-    const b = new Set();
-    let size = 0;
-
-    const ans  = new Array();
+    
+    const set = new Set();
+    const array = [];
 
     for(let i = 0; i < A.length; i++) {
         
-        a.add(A[i]);
-        b.add(B[i]);
-   
-        if(a.has(B[i]) ) c.add(B[i]);
-        if(b.has(A[i])) c.add(A[i])
+        set.add(B[i]);
+        let count = 0;
 
-        ans.push(c.size);
+        for(let j = 0; j <= i; j++) {
+            if(set.has(A[j])) count++;
+        }
+        array.push(count);
     }
 
-    return ans;
-    
+    return array;
 };
