be6eca2f7d8c4990e227857c64848a45ff60d840
Aman Burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2026-07-06 21:34:20 +0530

    Time: 11 ms (8.51%) | Memory: 60.3 MB (8.51%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var removeCoveredIntervals = function(intervals) {
+    
+    intervals.sort((a, b) => {
+        if(a[0] === b[0]) return b[1] - a[1];
+        return a[0] - b[0]
+    });
+    let count = 0;
+
+    for(let i = 0; i < intervals.length; i++) {
+
+        const [a, b] = intervals[i];
+        if(a === -1) continue;
+        for(let j = i + 1; j < intervals.length; j++) {
+            const [c, d] = intervals[j];
+            if(c === -1) continue;
+            if(a <= c && b >= d ) {
+                count++;
+                intervals[j][0] = -1;
+            }
+        }
+    }
+
+    return intervals.length - count;
+};
